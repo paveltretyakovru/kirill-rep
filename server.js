f7974af9fe4943883f14079c26b02ba14a1e5fec
Pavel Tretyakov <ptretyakov@roox.ru>
2018-12-15 14:12:24 +0300

    Installed socket.io to the server side. Updated server.js and reqired socket.io

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var io = require('socket.io')(app);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -7,4 +8,16 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+  socket.on('disconnect', function () {
+    io.emit('user disconnected');
+  });
 });
